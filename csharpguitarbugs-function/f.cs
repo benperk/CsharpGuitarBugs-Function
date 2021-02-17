5896477583843e8af1e0b3ee896752e4bdcdf19a
Benjamin Perkins <benperk@microsoft.com>
2021-02-17 14:39:46 +0100

    Update f.cs

--- csharpguitarbugs-function/f.cs
+++ csharpguitarbugs-function/f.cs
@@ -11,10 +11,11 @@ namespace csharpguitarbugs_function
     {
         [FunctionName("f")]
         public static void Run([CosmosDBTrigger(
-            databaseName: "cshaprguitar",
+            databaseName: "csharpguitar",
             collectionName: "elx",
             ConnectionStringSetting = "COSMOS_DB_CONNECTION_STRING",
-            LeaseCollectionName = "leases")]IReadOnlyList<Document> input, ILogger log)
+            LeaseCollectionName = "leases", 
+            CreateLeaseCollectionIfNotExists = true)]IReadOnlyList<Document> input, ILogger log)
         {
             List<string> random = new List<string>();
 
@@ -24,22 +25,35 @@ namespace csharpguitarbugs_function
                 log.LogInformation("First document Id " + input[0].Id);
                 Random r = new Random();
                 var n = r.Next(1, 100);
-                if (n % 10 == 0) throw new Microsoft.Azure.WebJobs.Host.FunctionTimeoutException("Something unexpected happened...help me fix it.");
+                if (n % 10 == 0)
+                {
+                    try
+                    {
+                        throw new Microsoft.Azure.WebJobs.Host.FunctionTimeoutException("Something unexpected happened...help me fix it.");
+                    }
+                    catch (Exception ex)
+                    {
+                        log.LogInformation($"Exception {ex.HResult}, message: {ex.Message}");
+                    }
+                }                
 
-                for (int i = 0; i < 2000; i++)
+                for (int i = 0; i < 1000; i++)
                 {
                     random.Add(System.Guid.NewGuid().ToString());
-                    System.Threading.Thread.Sleep(i);
-                    if (i % 100 == 0)
+                    var s = r.Next(10, 100);
+                    System.Threading.Thread.Sleep(s);
+                    if (i % 50 == 0)
                     {
                         log.LogInformation($"Just keeping you up to speed on the processing, still working, we are on number {i}");
                     }
                 }
+
+                if (n % 25 == 0) random.RemoveAt(1010);
             }
             random.Sort();
             random.Reverse();
             random.RemoveAt(500);
-            random.Reverse();
+            random.Reverse();            
         }
     }
 }
