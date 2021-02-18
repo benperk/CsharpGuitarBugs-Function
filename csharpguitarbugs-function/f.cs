dd46c3639bbb39b00e35d7f2b8a40df6f3c44546
Benjamin Perkins <benperk@microsoft.com>
2021-02-18 16:21:12 +0100

    Update f.cs

--- csharpguitarbugs-function/f.cs
+++ csharpguitarbugs-function/f.cs
@@ -21,8 +21,8 @@ namespace csharpguitarbugs_function
 
             if (input != null && input.Count > 0)
             {
-                log.LogInformation("Documents modified " + input.Count);
-                log.LogInformation("First document Id " + input[0].Id);
+                log.LogInformation($"Documents retrieved in this invocation is {input.Count}");
+                
                 Random r = new Random();
                 var n = r.Next(1, 100);
                 if (n % 10 == 0)
@@ -35,24 +35,66 @@ namespace csharpguitarbugs_function
                     {
                         log.LogInformation($"Exception {ex.HResult}, message: {ex.Message}");
                     }
-                }                
-
-                for (int i = 0; i < 1000; i++)
-                {
-                    random.Add(System.Guid.NewGuid().ToString());
-                    var s = r.Next(10, 100);
-                    System.Threading.Thread.Sleep(s);
-                    if (i % 50 == 0)
-                    {
-                        log.LogInformation($"Just keeping you up to speed on the processing, still working, we are on number {i}");
-                    }
                 }
 
-                if (n % 25 == 0) random.RemoveAt(1010);
+                string memoryTaker = "--WHAT-DOES-IMMUTABLE-MEAN";
+                foreach (var document in input)
+                {
+                    n = r.Next(1, 100);
+                    log.LogInformation($"Processing document with Id: {document.Id}");
+                    log.LogInformation($"SelfLink: {document.SelfLink}");
+                    log.LogInformation($"ResourceId: {document.ResourceId}");
+                    log.LogInformation($"AltLink: {document.AltLink}");
+                    log.LogInformation($"AttachmentsLink: {document.AttachmentsLink}");
+                    log.LogInformation($"ETag: {document.ETag}");
+                    log.LogInformation($"Timestamp: {document.Timestamp}");
+                    log.LogInformation($"TimeToLive: {document.TimeToLive}");
+
+                    if (n % 2 == 0)
+                    {
+                        for (int i = 0; i < 1000; i++)
+                        {
+                            memoryTaker += memoryTaker;
+                            System.Threading.Thread.Sleep(10);
+                            if (i % 50 == 0)
+                            {
+                                log.LogInformation($"We are on number: {i}, the length is: {memoryTaker.Length}");
+                            }
+                        }
+                        if (n % 4 == 0)
+                        {
+                            try
+                            {
+                                throw new Microsoft.Azure.WebJobs.Host.RecoverableException("A transient issue happened, retrying...");
+                            }
+                            catch (Exception ex)
+                            {
+                                log.LogInformation($"A '{ex.HResult}' exception happened with a message: '{ex.Message}' so will retry...");
+                                System.Threading.Thread.Sleep(1000);
+                                log.LogInformation("Success... :-)    on to the next document...");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 0; i < 1000; i++)
+                        {
+                            random.Add(System.Guid.NewGuid().ToString());
+                            var s = r.Next(10, 50);
+                            System.Threading.Thread.Sleep(s);
+                            if (i % 50 == 0)
+                            {
+                                log.LogInformation($"Just keeping you up to speed on the processing, still working, we are on number {i}");
+                            }
+                        }
+                    }
+
+                    if (n % 25 == 0) random.RemoveAt(1010);
+                }                
             }
             random.Sort();
             random.Reverse();
-            random.RemoveAt(500);
+            random.RemoveAt(50);
             random.Reverse();            
         }
     }
